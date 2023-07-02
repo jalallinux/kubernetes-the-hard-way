2f1d7da3c84de675b65776794e0fd34df2fae9b7
JalalLiinuX <smjjalalzadeh93@gmail.com>
2023-07-02 22:23:43 +0330

    update ips

--- configs/generators/run-worker.sh
+++ configs/generators/run-worker.sh
@@ -4,5 +4,7 @@ cd /app/configs
 sh /app/configs/generators/1-kubelet.sh
 sh /app/configs/generators/2-kube-proxy.sh
 
-mv /app/configs/worker-1.kubeconfig ~/
 mv /app/configs/kube-proxy.kubeconfig ~/
+for instance in worker-1 worker-2 worker-3; do
+  mv /app/configs/${instance}.kubeconfig ~/
+done
